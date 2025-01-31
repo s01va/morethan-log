d8d43a5cecdcf29a145b60432ed19b7f78613f4e
s01va <jinn0525@gmail.com>
2025-01-31 14:04:56 +0900

    project 미사용 주석변경

--- site.config.js
+++ site.config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      // name: `morethan-log`,
-      // href: "https://github.com/morethanmin/morethan-log",
+      name: ``,
+      href: "",
     },
   ],
   // blog setting (required)
